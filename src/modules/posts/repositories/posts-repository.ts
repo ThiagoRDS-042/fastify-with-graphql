a8f7e0397b827997abd70fd969bf3905bd587e93
Thiago Rodrigues <thiagords042@gmail.com>
2023-04-21 12:09:30 -0300

    feat: Add interface posts repo

---
+++ src/modules/posts/repositories/posts-repository.ts
@@ -0,0 +1,19 @@
+import { Post, PostCategoryType } from "../entities/post.entity";
+
+export interface IOptions {
+  titleContains?: string;
+  tagEquals?: string;
+  categoryEquals?: PostCategoryType;
+  publish?: boolean;
+  authorIdEquals?: string;
+}
+
+export interface PostsRepository {
+  create(post: Post): Promise<void>;
+  publish(postId: string): Promise<void>;
+  deprive(postId: string): Promise<void>;
+  save(post: Post): Promise<void>;
+  findById(postId: string): Promise<Post | null>;
+  findByTag(tag: string): Promise<Post | null>;
+  findMany(options?: IOptions): Promise<Post[]>;
+}
