bec7cb3d050ccfee5a296142f8b41b8f02297fe2
Thiago Rodrigues <thiagords042@gmail.com>
2023-04-23 11:11:44 -0300

    refactor: remove authors repository

--- src/modules/posts/use-cases/factories/make-update-post.ts
+++ src/modules/posts/use-cases/factories/make-update-post.ts
@@ -1,11 +1,9 @@
 import { UpdatePost } from "../update-post";
 import { PrismaPostsRepository } from "@modules/posts/infra/http/prisma/repositories/prisma-posts-repository";
-import { PrismaAuthorsRepository } from "@modules/authors/infra/prisma/repositories/prisma-authors-repository";
 
 export const makeUpdatePost = (): UpdatePost => {
   const postsRepository = new PrismaPostsRepository();
-  const authorsRepository = new PrismaAuthorsRepository();
-  const updatePost = new UpdatePost(postsRepository, authorsRepository);
+  const updatePost = new UpdatePost(postsRepository);
 
   return updatePost;
 };
