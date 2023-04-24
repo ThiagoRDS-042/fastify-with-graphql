79b9da867a0008355f705bc78da8bbe77b033f64
Thiago Rodrigues <thiagords042@gmail.com>
2023-04-24 17:45:34 -0300

    chore: configure mercurius context

--- src/@types/mercurius/index.d.ts
+++ src/@types/mercurius/index.d.ts
@@ -1,9 +1,11 @@
 import "mercurius";
 
 import { PostsDataLoader } from "@modules/posts/infra/http/graphql/dataloader";
+import { AuthorsDataLoader } from "@modules/authors/infra/http/graphql/dataloader";
 
 declare module "mercurius" {
   export interface MercuriusContext {
     postsDataLoader: PostsDataLoader;
+    authorsDataLoader: AuthorsDataLoader;
   }
 }
