e0fb07c7eebceed6667171b80952aed2caba33b3
Thiago Rodrigues <thiagords042@gmail.com>
2023-04-26 17:34:10 -0300

    chore: adjust tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./src", "!./src/schema.gql"],
+  entry: [
+    "./src/**",
+    "!./src/schema.gql",
+    "!./src/docs/diagrams/diagram_erd.md",
+  ],
   splitting: false,
   sourcemap: true,
   clean: true,
